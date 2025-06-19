c2aca6e8413225b17136997c1de062d39b4335e6
Damon McCullough <damonmcc1391@gmail.com>
2025-06-18 22:27:24 -0400

    Fix column selection in boroughs tables

--- stage_2/sql/build_boroughs.sql
+++ stage_2/sql/build_boroughs.sql
@@ -2,8 +2,8 @@ LOAD spatial;
 DROP TABLE IF EXISTS cleaned__boroughs;
 CREATE TABLE cleaned__boroughs AS
 SELECT
-    boro_name,
-    boro_code,
+    boroname as boro_name,
+    borocode as boro_code,
     ST_Transform(geom, 'EPSG:4326', 'ESRI:102718', always_xy := true) as geom,
   FROM
     borough_boundaries;
