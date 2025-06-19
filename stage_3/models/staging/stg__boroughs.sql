786abc34317290ff076ccfb765195c240924016b
Damon McCullough <damonmcc1391@gmail.com>
2025-06-19 12:49:29 -0400

    Add sqlfluff configuration and format Stage 3 SQL files

--- stage_3/models/staging/stg__boroughs.sql
+++ stage_3/models/staging/stg__boroughs.sql
@@ -1,5 +1,5 @@
 SELECT
-  boroname as boro_name,
-  borocode as boro_code,
-  ST_Transform(geom, 'EPSG:4326', 'ESRI:102718', always_xy := true) as geom,
+    boroname AS boro_name,
+    borocode AS boro_code,
+    ST_TRANSFORM(geom, 'EPSG:4326', 'ESRI:102718', always_xy := TRUE) AS geom,
 FROM {{ source("external_source", "Borough_Boundaries") }}
--- stage_3/models/staging/stg__markets.sql
+++ stage_3/models/staging/stg__markets.sql
@@ -1,23 +1,22 @@
 with markets as (
-  SELECT * FROM {{ source("external_source", "NYC_Farmers_Markets") }}
+    select *, from {{ source("external_source", "NYC_Farmers_Markets") }}
 )
 
-SELECT DISTINCT
-  "Market Name" as market_name,
-  "Accepts EBT" as accepts_ebt,
-  "Distributes Health Bucks?" as distributes_health_bucks,
-  (
-    CASE
-      WHEN "Open Year-Round" = 'Yes' THEN true
-      WHEN "Open Year-Round" = 'No' THEN false
-      ELSE NULL
-    END
-  ) as open_year_round,
-  "Street Address" as address,
-  "Borough" as borough,
-  "Latitude" as latitude,
-  "Longitude" as longitude,
-  ST_Point(Latitude, Longitude) as point_geometry_wgs84,
-  ST_Transform(point_geometry_wgs84, 'EPSG:4326', 'ESRI:102718') as geom,
-FROM
-  markets
+select distinct
+    "Market Name" as market_name,
+    "Accepts EBT" as accepts_ebt,
+    "Distributes Health Bucks?" as distributes_health_bucks,
+    "Street Address" as address,
+    borough,
+    latitude,
+    longitude,
+    (
+        case
+            when "Open Year-Round" = 'Yes' then TRUE
+            when "Open Year-Round" = 'No' then FALSE
+        end
+    ) as open_year_round,
+    ST_POINT(latitude, longitude) as point_geometry_wgs84,
+    ST_TRANSFORM(point_geometry_wgs84, 'EPSG:4326', 'ESRI:102718') as geom,
+from
+    markets
